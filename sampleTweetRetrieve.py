ec08dda233e9fbc30c97890248012c7a22ca8424
kblackerby <kblackerby2@gmail.com>
2016-02-19 12:11:21 -0600

    Update sampleTweetRetrieve.py

--- sampleTweetRetrieve.py
+++ sampleTweetRetrieve.py
@@ -11,9 +11,9 @@ import urllib2 as urllib
 
 # Credentials for me
 api_key = "OwqMMuILttYttVM9w5hlpgoIq"
-api_secret = "paMzscIu26GiLMOQKPYyZMqxZVMP2O10JxtUKrCVkQR2aEHvYo"
+api_secret = "**************************************************"
 access_token_key = "4870397840-aPrc7tXl6zAIS6AmSTelBcYSaVGEHo6lwulg5ag"
-access_token_secret = "oVdQPPJAh1tdekza6R4cetghmh1qrcFCLSGX0BpdumzKu"
+access_token_secret = "*********************************************"
 
 _debug = 0
 
