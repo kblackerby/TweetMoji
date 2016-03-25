0802db1ac53ac344f2755524ac654849551b589f
danmedsa <daniel.medina-sada@ttu.edu>
2016-03-24 22:23:46 -0500

    New Shell script
    Shell now runs the java file to split into individual tweets
    automatically every day.

--- Shell/tweetRetrieveShell.sh
+++ Shell/tweetRetrieveShell.sh
@@ -16,7 +16,21 @@ echo "File path: /home/dmedinas/Documents/Tweets/DTweets_`date +%Y%m%d`.json"
 echo ""
 echo "Collecting Tweets from `date +%D`..."
 echo ""
-python TexasTechTweetRetrieverPython.py > /home/dmedinas/Documents/Tweets/DTweets_`date +%Y%m%d`.json
+python TexasTechTweetRetrieverPython.py >> /home/dmedinas/Documents/Tweets/DTweets_`date +%Y%m%d`.json
+
+echo "Generating Individual JSON Files for file DTweets_`date +%Y%m%d -d "yesterday"`"
+
+javac IndividualTweetGenerator.java
+java IndividualTweetGenerator Tweets/DTweets_`date +%Y%m%d -d`.json
+
+echo ""
+echo ""
+echo ""
+echo "Generated Individual JSON Files for file DTweets_`date +%Y%m%d -d "yesterday"`"
+echo ""
+echo ""
+
+mv DTweets_`date +%Y%m%d -d "yesterday"` Individual_Tweets
 
 fi
 
